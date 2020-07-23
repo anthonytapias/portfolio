792dbe719d59b6f8012c4b11d6a822847eeb5664
project <anthonyatap@gmail.com>
2020-07-23 16:39:30 -0400

    First commit

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path
+
+
+
+
+urlpatterns = [
+
+    # path('', views.home_view, name='home'),
+    path('about/', views.about_view, name='about'),
+    path('projects/', views.projects_view, name='projects2')
+    ]
---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request):
+    template_name = "base.html"
+    return render(request, template_name)
+
+def about_view(request):
+    template_name = "about.html"
+    return render(request, template_name)
+
+def projects_view(request):
+    template_name = "projects2.html"
+    return render(request, template_name)
