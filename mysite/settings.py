ae626ab7a646f551c9b1fcd3d0671d2a621c104f
project <anthonyatap@gmail.com>
2020-07-26 20:40:08 -0400

    add files

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,11 +11,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
+
+#Activate Django-Heroku
+django_heroku.settings(locals())
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
